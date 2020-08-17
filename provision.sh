3060c5b64409e55934fc9b0ae7a1730c6392291d
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-17 21:28:57 +0300

    updated readme

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update -y
-sudo upgrade -y
+sudo apt upgrade -y
+sudo apt autoremove -y
 sudo apt install -y curl vim jq git make
 sudo curl -sL get.docker.com | sh
 sudo usermod -aG docker ubuntu
