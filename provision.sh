149037813cf74154ff3fd88fa21c2296977ff0ee
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-18 10:26:19 +0300

    add kubectl

--- provision.sh
+++ provision.sh
@@ -6,3 +6,8 @@ sudo apt install -y curl vim jq git make
 sudo curl -sL get.docker.com | sh
 sudo usermod -aG docker ubuntu
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+curl -LOs https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/bin
+
+
